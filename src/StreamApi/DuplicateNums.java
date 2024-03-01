4f9b64f81cf2718b5f51ff5c875902539701930e
kedarj24 <Mayback@2024>
2024-03-01 11:44:08 +0530

    Added duplicate nums example

---
+++ src/StreamApi/DuplicateNums.java
@@ -0,0 +1,21 @@
+package StreamApi;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DuplicateNums {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3,6,2,1,3,5,8,2,9);
+
+        Set<Integer> duplicate = numbers.stream().filter(e -> Collections.frequency(numbers, e) > 1)
+                .collect(Collectors.toSet());
+
+        System.out.println(duplicate);
+
+        //Collections.frequency(numbers, e): This method call returns the number of occurrences of the element e in the numbers list.
+    }
+}
